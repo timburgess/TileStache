b3d757dd1e0256fd59d1067b3822f7a08a51c3bb
Tim Burgess <timburgess@mac.com>
2016-08-21 10:38:30 +1000

    Write cached jpegs with .jpg extension. Whitespace cleanups.

--- TileStache/Caches.py
+++ TileStache/Caches.py
@@ -62,7 +62,7 @@ from . import S3
 
 def getCacheByName(name):
     """ Retrieve a cache object by name.
-    
+
         Raise an exception if the name doesn't work out.
     """
     if name.lower() == 'test':
@@ -87,9 +87,9 @@ def getCacheByName(name):
 
 class Test:
     """ Simple cache that doesn't actually cache anything.
-    
+
         Activity is optionally logged, though.
-    
+
         Example configuration:
 
             "cache": {
@@ -111,15 +111,15 @@ class Test:
         tile = '%(zoom)d/%(column)d/%(row)d' % coord.__dict__
 
         return ' '.join( (name, tile, format) )
-    
+
     def lock(self, layer, coord, format):
         """ Pretend to acquire a cache lock for this tile.
         """
         name = self._description(layer, coord, format)
-        
+
         if self.logfunc:
             self.logfunc('Test cache lock: ' + name)
-    
+
     def unlock(self, layer, coord, format):
         """ Pretend to release a cache lock for this tile.
         """
@@ -127,7 +127,7 @@ class Test:
 
         if self.logfunc:
             self.logfunc('Test cache unlock: ' + name)
-    
+
     def remove(self, layer, coord, format):
         """ Pretend to remove a cached tile.
         """
@@ -135,28 +135,28 @@ class Test:
 
         if self.logfunc:
             self.logfunc('Test cache remove: ' + name)
-    
+
     def read(self, layer, coord, format):
         """ Pretend to read a cached tile.
         """
         name = self._description(layer, coord, format)
-        
+
         if self.logfunc:
             self.logfunc('Test cache read: ' + name)
 
         return None
-    
+
     def save(self, body, layer, coord, format):
         """ Pretend to save a cached tile.
         """
         name = self._description(layer, coord, format)
-        
+
         if self.logfunc:
             self.logfunc('Test cache save: %d bytes to %s' % (len(body), name))
 
 class Disk:
     """ Caches files to disk.
-    
+
         Example configuration:
 
             "cache": {
@@ -191,46 +191,46 @@ class Disk:
 
     def _is_compressed(self, format):
         return format.lower() in self.gzip
-    
+
     def _filepath(self, layer, coord, format):
         """
         """
         l = layer.name()
         z = '%d' % coord.zoom
-        e = format.lower()
+        e = 'jpg' if format.lower() == 'jpeg' else format.lower()
         e += self._is_compressed(format) and '.gz' or ''
-        
+
         if self.dirs == 'safe':
             x = '%06d' % coord.column
             y = '%06d' % coord.row
 
             x1, x2 = x[:3], x[3:]
             y1, y2 = y[:3], y[3:]
-            
+
             filepath = os.sep.join( (l, z, x1, x2, y1, y2 + '.' + e) )
-            
+
         elif self.dirs == 'portable':
             x = '%d' % coord.column
             y = '%d' % coord.row
 
             filepath = os.sep.join( (l, z, x, y + '.' + e) )
-            
+
         elif self.dirs == 'quadtile':
             pad, length = 1 << 31, 1 + coord.zoom
 
             # two binary strings, one per dimension
             xs = bin(pad + int(coord.column))[-length:]
             ys = bin(pad + int(coord.row))[-length:]
-            
+
             # interleave binary bits into plain digits, 0-3.
             # adapted from ModestMaps.Tiles.toMicrosoft()
             dirpath = ''.join([str(int(y+x, 2)) for (x, y) in zip(xs, ys)])
-            
+
             # built a list of nested directory names and a file basename
             parts = [dirpath[i:i+3] for i in range(0, len(dirpath), 3)]
-            
+
             filepath = os.sep.join([l] + parts[:-1] + [parts[-1] + '.' + e])
-        
+
         else:
             raise KnownUnknown('Please provide a valid "dirs" parameter to the Disk cache, either "safe", "portable" or "quadtile" but not "%s"' % self.dirs)
 
@@ -248,21 +248,21 @@ class Disk:
         """
         """
         return self._fullpath(layer, coord, format) + '.lock'
-    
+
     def lock(self, layer, coord, format):
         """ Acquire a cache lock for this tile.
-        
+
             Returns nothing, but blocks until the lock has been acquired.
             Lock is implemented as an empty directory next to the tile file.
         """
         lockpath = self._lockpath(layer, coord, format)
         due = time.time() + layer.stale_lock_timeout
-        
+
         while True:
             # try to acquire a directory lock, repeating if necessary.
             try:
                 umask_old = os.umask(self.umask)
-                
+
                 if time.time() > due:
                     # someone left the door locked.
                     try:
@@ -270,7 +270,7 @@ class Disk:
                     except OSError:
                         # Oh - no they didn't.
                         pass
-                
+
                 os.makedirs(lockpath, 0777&~self.umask)
                 break
             except OSError, e:
@@ -279,7 +279,7 @@ class Disk:
                 time.sleep(.2)
             finally:
                 os.umask(umask_old)
-    
+
     def unlock(self, layer, coord, format):
         """ Release a cache lock for this tile.
 
@@ -292,44 +292,44 @@ class Disk:
         except OSError:
             # Ok, someone else deleted it already
             pass
-        
+
     def remove(self, layer, coord, format):
         """ Remove a cached tile.
         """
         fullpath = self._fullpath(layer, coord, format)
-        
+
         try:
             os.remove(fullpath)
         except OSError, e:
             # errno=2 means that the file does not exist, which is fine
             if e.errno != 2:
                 raise
-        
+
     def read(self, layer, coord, format):
         """ Read a cached tile.
         """
         fullpath = self._fullpath(layer, coord, format)
-        
+
         if not exists(fullpath):
             return None
 
         age = time.time() - os.stat(fullpath).st_mtime
-        
+
         if layer.cache_lifespan and age > layer.cache_lifespan:
             return None
-    
+
         elif self._is_compressed(format):
             return gzip.open(fullpath, 'r').read()
 
         else:
             body = open(fullpath, 'rb').read()
             return body
-    
+
     def save(self, body, layer, coord, format):
         """ Save a cached tile.
         """
         fullpath = self._fullpath(layer, coord, format)
-        
+
         try:
             umask_old = os.umask(self.umask)
             os.makedirs(dirname(fullpath), 0777&~self.umask)
@@ -343,7 +343,7 @@ class Disk:
         suffix += self._is_compressed(format) and '.gz' or ''
 
         fh, tmp_path = mkstemp(dir=self.cachepath, suffix=suffix)
-        
+
         if self._is_compressed(format):
             os.close(fh)
             tmp_file = gzip.open(tmp_path, 'w')
@@ -352,7 +352,7 @@ class Disk:
         else:
             os.write(fh, body)
             os.close(fh)
-        
+
         try:
             os.rename(tmp_path, fullpath)
         except OSError:
@@ -363,15 +363,15 @@ class Disk:
 
 class Multi:
     """ Caches tiles to multiple, ordered caches.
-        
+
         Multi cache is well-suited for a speed-to-capacity gradient, for
         example a combination of Memcache and S3 to take advantage of the high
         speed of memcache and the high capacity of S3. Each tier of caching is
         checked sequentially when reading from the cache, while all tiers are
         used together for writing. Locks are only used with the first cache.
-        
+
         Example configuration:
-        
+
             "cache": {
               "name": "Multi",
               "tiers": [
@@ -387,7 +387,7 @@ class Multi:
             }
 
         Multi cache parameters:
-        
+
           tiers
             Required list of cache configurations. The fastest, most local
             cache should be at the beginning of the list while the slowest or
@@ -400,44 +400,44 @@ class Multi:
 
     def lock(self, layer, coord, format):
         """ Acquire a cache lock for this tile in the first tier.
-        
+
             Returns nothing, but blocks until the lock has been acquired.
         """
         return self.tiers[0].lock(layer, coord, format)
-    
+
     def unlock(self, layer, coord, format):
         """ Release a cache lock for this tile in the first tier.
         """
         return self.tiers[0].unlock(layer, coord, format)
-        
+
     def remove(self, layer, coord, format):
         """ Remove a cached tile from every tier.
         """
         for (index, cache) in enumerate(self.tiers):
             cache.remove(layer, coord, format)
-        
+
     def read(self, layer, coord, format):
         """ Read a cached tile.
-        
+
             Start at the first tier and work forwards until a cached tile
             is found. When found, save it back to the earlier tiers for faster
             access on future requests.
         """
         for (index, cache) in enumerate(self.tiers):
             body = cache.read(layer, coord, format)
-            
+
             if body:
                 # save the body in earlier tiers for speedier access
                 for cache in self.tiers[:index]:
                     cache.save(body, layer, coord, format)
-                
+
                 return body
-        
+
         return None
-    
+
     def save(self, body, layer, coord, format):
         """ Save a cached tile.
-        
+
             Every tier gets a saved copy.
         """
         for (index, cache) in enumerate(self.tiers):
